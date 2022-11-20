a1833755c8e196feb54259ac4c8132487cfaa7e0
_SARTH21_ <97116457+sarthak4399@users.noreply.github.com>
2022-11-20 14:44:34 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import "./App.css";
 const App = () => {
   return (
-    <div className=" z-1 mt-[20%] bg-blend-screen text-[rgb(198,255,189)]">
-      <div className="w-20 mt-[-10rem] justify-items-center  ml-40 absolute z-20">
+    <div className=" z-1 mt-[20%] bg-blend-screen text-[rgb(198,255,189)] justify-center">
+      <div className="w-20 mt-[-10rem] ml-40 absolute z-20">
         <h1 className="  text-3xl font-bold text-[rgb(198,255,189)]">RNXG</h1>
       </div>
       <video
@@ -30,13 +30,13 @@ const App = () => {
       <div className=" ml-[45%] py-10 ">
         <button
           type="button"
-          class="inline-block px-6 py-2 border-2 group border-[rgb(198,255,189)] text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out absolute md:ml-[40]"
+          class="inline-block px-6 py-2 border-2 group sm:justify-items-center sm:justify-center border-[rgb(198,255,189)] text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out absolute md:ml-[40]"
           href="https://youtu.be/MAadWJdVLpk"
         >
           <a
             href="https://youtu.be/MAadWJdVLpk"
             target="_blank"
-            className=" group text-white text-sm "
+            className=" group text-white text-sm  sm:justify-items-center sm:justify-center"
           >
             TEASER
           </a>
