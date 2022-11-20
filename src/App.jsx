d68ccb2c00adf2c7d36449bd71d522e834538ab0
_SARTH21_ <97116457+sarthak4399@users.noreply.github.com>
2022-11-20 13:28:01 +0530

    update1

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import Bg from "../src/assets/video (1).mp4";
 import "./App.css";
 const App = () => {
   return (
-    <div>
-      <video id="myVideo" src="https://player.vimeo.com/video/369267842" type="video/mp4" autoPlay loop></video>
-
-      <div className=" z-10 mt-[20%] bg-blend-screen text-[#00df9a]">
-        <FlipCountdown
-          className="backdrop-blur-lg"
-          dayTitle="Days "
-          hourTitle="Hours"
-          minuteTitle="Minutes"
-          secondTitle="Seconds"
-          endAt={"21 November 2022, 18:15:00"}
-        />
+    <div className=" z-10 mt-[20%] bg-blend-screen text-[#00df9a]">
+      <video
+        id="myVideo"
+        src={Bg}
+        type="video/mp4"
+        className="object-cover"
+      ></video>
+      <FlipCountdown
+        className="backdrop-blur-lg"
+        dayTitle="Days "
+        hourTitle="Hours"
+        minuteTitle="Minutes"
+        secondTitle="Seconds"
+        endAt={"21 November 2022, 18:15:00"}
+      />
+      <div>
+        <div></div>
       </div>
     </div>
   );
